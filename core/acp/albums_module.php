2baf6873a9b7bf30df9dff84c70b5067961f11ad
Leinad4Mind <leinad4mind@protonmail.com>
2026-08-03 12:55:19 +0100

    refactor: remove unused contest table globals
    (cherry picked from commit e7f7798bc1e7b67b3921289dff4ffdd5d198c845)

--- core/acp/albums_module.php
+++ core/acp/albums_module.php
@@ -40,7 +40,6 @@ class albums_module
 
 		// Let us define some helpers;
 		$albums_table = $table_prefix . 'gallery_albums';
-		$contests_table = $table_prefix . 'gallery_contests';
 		// Init ext gallery
 		$this->language->add_lang(['gallery_acp', 'gallery'], 'phpbbgallery/core');
 
--- core/acp/main_module.php
+++ core/acp/main_module.php
@@ -66,7 +66,6 @@ class main_module
 		$roles_table = $table_prefix . 'gallery_roles';
 		$permissions_table = $table_prefix . 'gallery_permissions';
 		$modscache_table = $table_prefix . 'gallery_modscache';
-		$contests_table = $table_prefix . 'gallery_contests';
 		$users_table = $table_prefix . 'gallery_users';
 		$images_table = $table_prefix . 'gallery_images';
 		// Init album
