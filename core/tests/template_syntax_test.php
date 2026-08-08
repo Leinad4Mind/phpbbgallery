455290a10d7190470a5c16fe659702b2f6b7dafc
Leinad4Mind <leinad4mind@protonmail.com>
2026-08-08 02:20:05 +0100

    Update Gallery template inventory test
    (cherry picked from commit ae4068f762493485a7a2fe9820fb5d17cac8c1bf)

--- core/tests/template_syntax_test.php
+++ core/tests/template_syntax_test.php
@@ -22,7 +22,7 @@ final class template_syntax_test extends TestCase
 	public function test_modernized_templates_use_only_native_twig_syntax(): void
 	{
 		$template_paths = $this->template_paths();
-		$this->assertCount(189, $template_paths);
+		$this->assertCount(192, $template_paths);
 
 		foreach ($template_paths as $template_path)
 		{
