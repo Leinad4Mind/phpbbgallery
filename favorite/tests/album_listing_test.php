70b1e7ef529276b817da1e62c5c023d50401cd35
Leinad4Mind <leinad4mind@protonmail.com>
2026-08-08 16:25:29 +0100

    Center Gallery favorite heart icons
    (cherry picked from commit 1119c0ff626437765c30a566a4d21e47e306cfc4)

--- favorite/tests/album_listing_test.php
+++ favorite/tests/album_listing_test.php
@@ -59,6 +59,14 @@ final class album_listing_test extends TestCase
 		$this->assertStringContainsString('data-toggle-url="{{ image.U_FAVORITE_IMAGE_TOGGLE }}"', $template);
 		$this->assertStringContainsString('class="gallery-favorite-icon icon fa ', $template);
 		$this->assertStringContainsString('.gallery-favorite-icon.fa-heart', $stylesheet);
+		$this->assertMatchesRegularExpression(
+			'/\.gallery-favorite-icon\s*\{[^}]*align-items:\s*center;[^}]*display:\s*flex;[^}]*height:\s*100%;[^}]*justify-content:\s*center;[^}]*width:\s*100%;[^}]*\}/s',
+			$stylesheet
+		);
+		$this->assertMatchesRegularExpression(
+			'/\.gallery-favorite-icon::before\s*\{[^}]*display:\s*block;[^}]*line-height:\s*1;[^}]*\}/s',
+			$stylesheet
+		);
 		$this->assertStringContainsString("INCLUDECSS '@phpbbgallery_favorite/favorite.css'", $head);
 		$this->assertStringContainsString("INCLUDEJS '@phpbbgallery_favorite/favorite.js'", $head);
 	}
